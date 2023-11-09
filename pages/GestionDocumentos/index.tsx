a4efa1a6622bf5a7be05c88f2edc4bd659aa9710
MauricioCombariza <mcombarizav@gmail.com>
2023-11-09 13:45:35 -0500

    Commit cambiando axios por fetch

--- pages/GestionDocumentos/index.tsx
+++ pages/GestionDocumentos/index.tsx
@@ -1,7 +1,6 @@
 import { Layout } from "../../components/Layout";
 import { useState } from 'react';
 import Container from '@mui/material/Container';
-import axios from "axios";
 import { useAuth } from '../../Auth';
 import { LoadingSpinner } from "../../components/LoadingSpinner";  // Ajusta la ruta a tu ubicación real del componente
 
@@ -16,30 +15,30 @@ function GestionDocumentos() {
       setLoading(true); // Muestra el spinner mientras se realiza la descarga
 
       const token = auth.token;
-      const baseApi = process.env.API
-      // const API = 'https://servilla-server-api.onrender.com/document/documentos/gestion'
-      // const API = 'http://127.0.0.1:8000/document/documentos/gestion'
-      const API = `${baseApi}/document/documentos/gestion`
+      const baseApi = process.env.API;
+      const API = `${baseApi}/document/documentos/gestion`;
 
-      const response = await axios.get(API, {
-        params: {
-          fecha_inicial: fechaInicial,
-          fecha_final: fechaFinal,
-        },
-        responseType: 'blob',
+      const queryParams = new URLSearchParams({
+        fecha_inicial: fechaInicial,
+        fecha_final: fechaFinal,
+      });
+
+      const url = `${API}?${queryParams.toString()}`;
+      const response = await fetch(url, {
+        method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
 
-      const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-      const url = window.URL.createObjectURL(blob);
+      const blob = await response.blob();
+      const urlBlob = window.URL.createObjectURL(blob);
       const a = document.createElement('a');
-      a.href = url;
+      a.href = urlBlob;
       a.download = 'gestion.xlsx';
       a.click();
 
-      window.URL.revokeObjectURL(url);
+      window.URL.revokeObjectURL(urlBlob);
     } catch (error) {
       console.error('Error al descargar el archivo', error);
     } finally {
@@ -50,39 +49,38 @@ function GestionDocumentos() {
   return (
     <Layout>
       <Container maxWidth="md">
-  <div className="mt-10 p-4 bg-gray-200 text-center">
-    <h1 className="font-bold text-ser text-3xl mb-10">Descargar Archivo de Gestión</h1>
-    <div className="flex flex-col items-center mb-10">
-      <label htmlFor="fechaInicial" className="mb-2">Fecha Inicial(aaaa.mm.dd):</label>
-      <input
-        type="text"
-        id="fechaInicial"
-        value={fechaInicial}
-        onChange={(e) => setFechaInicial(e.target.value)}
-        className="w-full py-2 px-4 bg-green-600 rounded border border-darkser hover:bg-ser focus:outline-none focus:shadow-outline hover:text-white"
-      />
-      <label htmlFor="fechaFinal" className="mt-4 mb-2">Fecha Final(aaaa.mm.dd):</label>
-      <input
-        type="text"
-        id="fechaFinal"
-        value={fechaFinal}
-        onChange={(e) => setFechaFinal(e.target.value)}
-        className="w-full py-2 px-4 bg-green-600 rounded border border-darkser hover:bg-ser focus:outline-none focus:shadow-outline hover:text-white"
-      />
-    </div>
-    <button
-      onClick={handleDescargarArchivo}
-      disabled={loading}
-      className="bg-green-600 text-white py-2 px-4 rounded hover:bg-ser focus:outline-none focus:shadow-outline"
-    >
-      Descargar Archivo
-    </button>
-    {loading && <LoadingSpinner />} {/* Muestra el spinner si 'loading' es true */}
-  </div>
-</Container>
-
+        <div className="mt-10 p-4 bg-gray-200 text-center">
+          <h1 className="font-bold text-ser text-3xl mb-10">Descargar Archivo de Gestión</h1>
+          <div className="flex flex-col items-center mb-10">
+            <label htmlFor="fechaInicial" className="mb-2">Fecha Inicial(aaaa.mm.dd):</label>
+            <input
+              type="text"
+              id="fechaInicial"
+              value={fechaInicial}
+              onChange={(e) => setFechaInicial(e.target.value)}
+              className="w-full py-2 px-4 bg-green-600 rounded border border-darkser hover:bg-ser focus:outline-none focus:shadow-outline hover:text-white"
+            />
+            <label htmlFor="fechaFinal" className="mt-4 mb-2">Fecha Final(aaaa.mm.dd):</label>
+            <input
+              type="text"
+              id="fechaFinal"
+              value={fechaFinal}
+              onChange={(e) => setFechaFinal(e.target.value)}
+              className="w-full py-2 px-4 bg-green-600 rounded border border-darkser hover:bg-ser focus:outline-none focus:shadow-outline hover:text-white"
+            />
+          </div>
+          <button
+            onClick={handleDescargarArchivo}
+            disabled={loading}
+            className="bg-green-600 text-white py-2 px-4 rounded hover:bg-ser focus:outline-none focus:shadow-outline"
+          >
+            Descargar Archivo
+          </button>
+          {loading && <LoadingSpinner />} {/* Muestra el spinner si 'loading' es true */}
+        </div>
+      </Container>
     </Layout>
   );
 }
 
-export default GestionDocumentos ;
+export default GestionDocumentos;
