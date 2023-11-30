7218ac3e61f586515cff0cf88f1380a570b3463c
MauricioCombariza <mcombarizav@gmail.com>
2023-11-30 11:05:30 -0500

    Commit agregando google tag mannager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,28 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Head>
+      {/* Google Tag Manager */}
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MZBMVT97');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Tag Manager - Sección del body */}
+          <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-GTM-MZBMVT97" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {/* End Google Tag Manager - Sección del body */}
+
         <link rel="icon" type="image/svg+xml" href="https://res.cloudinary.com/combariza/image/upload/v1695061362/Servilla/servilla_favicon.png" />
         <title>Servilla S.A.S - Solución logística</title>
         <meta name="description" content="Empresa dedicada al envio de paquetes y documentos, en Colombia, desde 1974" />
