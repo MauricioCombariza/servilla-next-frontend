d002a663d192dcc79d131438c4bd9753cd1dc580
MauricioCombariza <mcombarizav@gmail.com>
2024-01-15 06:53:35 -0500

    Commit con Ana más build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+// /** @type {import('next').NextConfig} */
+
+// module.exports = {
+//   nextConfig: {
+//     reactStrictMode: true,
+//   },
+//   env: {
+//     API: process.env.API,
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  nextConfig: {
-    reactStrictMode: true,
-  },
+  reactStrictMode: true,
   env: {
     API: process.env.API,
   },
