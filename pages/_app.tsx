a268733d8e6d1928968dccc767fedd0b6b13efd0
MauricioCombariza <mcombarizav@gmail.com>
2024-02-27 16:27:22 -0500

    commit con Tag Mannager analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,33 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import Head from 'next/head';
-import { AuthProvider } from '@/Auth'
-import type { AppProps } from 'next/app'
-import { NavBar } from '@/components/NavBar/NavBarTailwind'
+import { AuthProvider } from '@/Auth';
+import type { AppProps } from 'next/app';
+import { NavBar } from '@/components/NavBar/NavBarTailwind';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Head>
-      {/* Google Tag Manager */}
-      <script
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JCQST79BSL"></script>
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MZBMVT97');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JCQST79BSL');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
         {/* Google Tag Manager - Sección del body */}
-          <noscript
+        <noscript
           dangerouslySetInnerHTML={{
             __html: `
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-GTM-MZBMVT97" height="0" width="0" style="display:none;visibility:hidden"></iframe>
@@ -30,16 +36,16 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         {/* End Google Tag Manager - Sección del body */}
         {/* <!-- Start of HubSpot Embed Code --> */}
-          <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/45377115.js"></script>
+        <script async defer src="//js.hs-scripts.com/45377115.js"></script>
         {/* <!-- End of HubSpot Embed Code --> */}
 
         <link rel="icon" type="image/svg+xml" href="https://res.cloudinary.com/combariza/image/upload/v1695061362/Servilla/servilla_favicon.png" />
         <title>Servilla S.A.S - Solución logística</title>
-        <meta name="description" content="Empresa dedicada al envio de paquetes y documentos, en Colombia, desde 1974" />
-        <meta name="keywords" content="paqueteria, envíos, logística, mensajeria, distribución" />
+        <meta name="description" content="Empresa dedicada al envío de paquetes y documentos, en Colombia, desde 1974" />
+        <meta name="keywords" content="paquetería, envíos, logística, mensajería, distribución" />
       </Head>
       <NavBar />
       <Component {...pageProps} />
     </AuthProvider>
-  )
+  );
 }
