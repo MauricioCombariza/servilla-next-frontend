dffa2f0f70a39c2c37be922f8c2a36af2cdb0f74
MauricioCombariza <mcombarizav@gmail.com>
2024-08-09 14:06:02 -0500

    commit "Google analytics"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,39 +45,6 @@ const MyApp: FC<MyAppProps> = ({ Component, pageProps }) => {
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
-      
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-JCQST79BSL"
-          strategy="lazyOnload"
-        />
-        <Script
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-JCQST79BSL');
-            `,
-          }}
-        />
-        {/* Google Tag Manager - Sección del body */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-GTM-MZBMVT97" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
-        {/* End Google Tag Manager - Sección del body */}
-        {/* Start of HubSpot Embed Code */}
-        <Script
-          src="//js.hs-scripts.com/45377115.js"
-          strategy="lazyOnload"
-        />
-        {/* End of HubSpot Embed Code */}
-
         <link rel="icon" type="image/svg+xml" href="https://res.cloudinary.com/combariza/image/upload/v1695061362/Servilla/servilla_favicon.png" />
         <title>Servilla S.A.S - Solución logística</title>
         <meta name="description" content="Empresa dedicada al envío de paquetes y documentos, en Colombia, desde 1974" />
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,16 @@
-const CACHE_NAME = 'my-app-cache-v1';
+const CACHE_NAME = 'servilla-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/components/Image/icons/apple-touch-icon.png',
-  '/src/js/material.min.js',
-  '/src/css/material.min.css',
+  '/Home',
+  '/Servicio/Distribucion',
+  '/Servicio/Paqueteo',
+  '/Servicio/Fulfillment',
+  '/Servicio/Alistamiento',
+  '/Servicio/Dropshipping',
+  '/Aplicacion',
+  '/Contactenos',
+  '/Ingresar',
+  '/Registrarse',
   // Añade aquí otras rutas que quieras cachear
 ];
 
@@ -13,10 +19,19 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .catch(error => {
-        console.error('Error al agregar recursos al caché', error);
+        return Promise.all(
+          urlsToCache.map(url => {
+            return fetch(url).then(response => {
+              if (!response.ok) {
+                console.error(`Error al cargar el recurso: ${url} - Status: ${response.status}`);
+                throw new Error(`Error al cargar el recurso: ${url} - Status: ${response.status}`);
+              }
+              return cache.put(url, response);
+            }).catch(error => {
+              console.error(`Error al intentar cargar el recurso: ${url}`, error);
+            });
+          })
+        );
       })
   );
 });
@@ -28,24 +43,11 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
       );
     })
-  );
-});
-
-// Intercepción de las solicitudes de red
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(error => {
-        console.error('Error al responder desde el caché o la red', error);
-      })
   );
 });
