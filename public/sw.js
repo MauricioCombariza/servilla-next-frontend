2829ef5cd93492f55b4b6dfc53e36f0a3bc3c547
MauricioCombariza <mcombarizav@gmail.com>
2024-08-14 14:24:35 -0500

    commit "pwa offline"

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'my-app-cache-v1';
+const CACHE_NAME = 'servilla-app-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/components/Image/icons/apple-touch-icon.png',
-  '/src/js/material.min.js',
-  '/src/css/material.min.css',
+  '/Contactenos',
+  '/Ingresar',
+  '/Registrarse',
+  '/Mision',
+  '/Vision',
+  '/Calidad',
+  '/Servicio/Distribucion',
+  '/Servicio/Paqueteo',
+  '/Servicio/Dropshipping',
+  '/Servicio/Email',
+  '/Servicio/Fullfilment',
+  '/Servicio/Alistamiento',
+  '/Aplicacion',
+  'https://res.cloudinary.com/combariza/image/upload/v1695061362/Servilla/servilla_favicon.png',
+  'https://res.cloudinary.com/combariza/image/upload/c_crop,g_center,z_0.5/v1643213357/Servilla/delivery3_imei5v.webp',
+  'https://res.cloudinary.com/combariza/image/upload/,c_limit/v1643213349/Servilla/delivery2_djawkh.jpg',
+  'https://res.cloudinary.com/combariza/image/upload/c_scale,w_auto/v1720441774/Servilla/inventario.jpg',
+  'https://res.cloudinary.com/combariza/image/upload/c_scale,w_auto/v1720442450/Servilla/empaque.jpg',
+  'https://res.cloudinary.com/combariza/image/upload/c_scale,w_auto/v1720447327/Servilla/dropshipping.jpg',
   // Añade aquí otras rutas que quieras cachear
 ];
 
@@ -17,6 +32,26 @@ self.addEventListener('install', event => {
       })
       .catch(error => {
         console.error('Error al agregar recursos al caché', error);
+      })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            return caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, response.clone());
+                return response;
+              });
+          });
       })
   );
 });
